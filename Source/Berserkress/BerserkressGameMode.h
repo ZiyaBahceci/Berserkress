71efe5a7afec1d92c67b6158bd62ca4334f1960f
hngur <45761081+ZiyaBahceci@users.noreply.github.com>
2023-05-09 16:12:21 +0300

    Config init

---
+++ Source/Berserkress/BerserkressGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BerserkressGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABerserkressGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABerserkressGameMode();
+};
+
+
+
