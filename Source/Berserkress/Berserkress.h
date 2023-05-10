b66792348f2f92212d8a1cc7e4dc9450ab090fdd
hngur <45761081+ZiyaBahceci@users.noreply.github.com>
2023-05-10 15:17:18 +0300

    Starting

--- Source/Berserkress/Berserkress.h
+++ Source/Berserkress/Berserkress.h
@@ -3,3 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 250.f, THIRTY, FColor::Red, true);
+#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Blue, true, -1.f, 0, 5.F);
+	
--- Source/Berserkress/Private/Item.cpp
+++ Source/Berserkress/Private/Item.cpp
@@ -2,11 +2,14 @@
 
 
 #include "Item.h"
+#include "DrawDebugHelpers.h"
+#include "Berserkress/Berserkress.h"
+
+#define THIRTY 30
 
 // Sets default values
 AItem::AItem()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -14,12 +17,35 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("BeginPlayCalled!"));
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlayCalled!")); 
+	
+	UWorld* World = GetWorld();
+
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	
+	DRAW_SPHERE(Location);
+	DRAW_LINE(Location, Location + Forward * 2000.f);
+	SetActorLocation(FVector(-620.f, 0, 250.f));	
+	SetActorRotation(FRotator(90.f, 45.f, 22.f));
+
+	Location = GetActorLocation();
+	Forward = GetActorForwardVector();
+	DRAW_SPHERE(Location);
+	DRAW_LINE(Location, Location + Forward * 2000.f);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-}
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);	
 
+	if (GEngine)
+	{
+		FString Name = GetName();	 
+		FString Message = FString::Printf(TEXT("DeltaTime = %f"), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(1, 25.f, FColor::Cyan, Message);
+	}
+
+}
