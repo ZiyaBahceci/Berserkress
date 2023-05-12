b0e08989f2301061870ee2688541e4cb5534db98
hngur <45761081+ZiyaBahceci@users.noreply.github.com>
2023-05-12 13:25:35 +0300

    Input Update (WIP)

--- Source/Berserkress/Private/Pawn/Bird.cpp
+++ Source/Berserkress/Private/Pawn/Bird.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Pawn/Bird.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 
@@ -14,23 +17,58 @@ SetRootComponent(Capsule);
 
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
 	BirdMesh->SetupAttachment(GetRootComponent());
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;		
 }
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	/*
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	if(PlayerController)
+	{
+	UEnhancedInputLocalPlayerSubsystem*  Subsystem =ULocalPlayer::GetSubsystem<	UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if(Subsystem)
+		{
+				
+		}
+	}
+	*/
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	}
 }
+
+void ABird::Move(const FInputActionValue& Value)
+{
+	const bool CurrentValue = Value.Get<bool>();
+	if(CurrentValue)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("IA_Move triggered"));
+	}
+}
+
 
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-}
+}	
 
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+	}
 }
 
--- Source/Berserkress/Public/Pawn/Bird.h
+++ Source/Berserkress/Public/Pawn/Bird.h
@@ -3,16 +3,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputMappingQuery.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "Bird.generated.h"
 
 class UCapsuleComponent;
 class USkeletalMeshComponent;
+class UInputAction;
 
 UCLASS()
 class BERSERKRESS_API ABird : public APawn
 {
-	GENERATED_BODY()
+	GENERATED_BODY() 
 
 public:
 	ABird();
@@ -21,6 +24,17 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+UInputMappingContext* BirdMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
+	void Move(const FInputActionValue& Value);
+
+	void Look(const FInputActionValue& Value);
+	
 private:
 	
 	UPROPERTY(VisibleAnywhere)
