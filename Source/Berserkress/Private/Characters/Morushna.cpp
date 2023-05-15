0adad0d036f1b07e21b9300fc111b0f6e27c51d9
hngur <45761081+ZiyaBahceci@users.noreply.github.com>
2023-05-15 10:36:35 +0300

    Morushna movement input

--- Source/Berserkress/Private/Characters/Morushna.cpp
+++ Source/Berserkress/Private/Characters/Morushna.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Characters/Morushna.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 AMorushna::AMorushna()
 {
@@ -12,6 +15,33 @@ AMorushna::AMorushna()
 void AMorushna::BeginPlay()
 {
 	Super::BeginPlay();
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MorushnaContext, 0);
+		}
+	}
+	
+}
+
+void AMorushna::Move(const FInputActionValue& Value)
+{
+	const FVector2d MovementVector = Value.Get<FVector2d>();
+	/*
+	const FVector Forward = GetActorForwardVector();
+	AddMovementInput(Forward, MovementVector.Y);
+	const FVector Right = GetActorRightVector();
+	AddMovementInput(Right, MovementVector.X);
+	*/
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirection, MovementVector.X);
 	
 }
 
@@ -25,5 +55,11 @@ void AMorushna::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+{
+	EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &AMorushna::Move);
+	
+}
+	
 }
 
--- Source/Berserkress/Public/Characters/Morushna.h
+++ Source/Berserkress/Public/Characters/Morushna.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "Morushna.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class BERSERKRESS_API AMorushna : public ACharacter
@@ -16,7 +20,16 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void Tick(float DeltaTime) override;
 
+
+
 protected:
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputMappingContext* MorushnaContext;
 
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* MovementAction;	
+
+	void Move(const FInputActionValue& Value);
 };
