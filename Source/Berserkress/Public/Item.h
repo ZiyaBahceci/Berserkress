b0f5ac54cf8743a8cd5e1cd75c3bc91cfbf7cc3b
hngur <45761081+ZiyaBahceci@users.noreply.github.com>
2023-05-16 10:43:45 +0300

    Weapons/Items

--- Source/Berserkress/Public/Item.h
+++ Source/Berserkress/Public/Item.h
@@ -37,8 +37,8 @@ protected:
 	float TestFloat = 29.f;
 
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
 
 	UFUNCTION()
-	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
--- Source/Berserkress/Public/Weapons/Weapon.h
+++ Source/Berserkress/Public/Weapons/Weapon.h
@@ -13,5 +13,15 @@ UCLASS()
 class BERSERKRESS_API AWeapon : public AItem
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+	{
+		Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	}
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+	{
+		Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+	}
 };
+
